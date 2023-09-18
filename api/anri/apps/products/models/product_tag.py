860fcc8b10a197dfff4f1b32931bc88d021f1c33
Иван Бойко <63643593+ivanboika@users.noreply.github.com>
2023-09-18 21:09:15 +0300

    deleted product's app models

--- api/anri/apps/products/models/product_tag.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# from anri.apps.products.models.tags import Tag
-from anri.apps.common.models import CoreModel
-
-
-class ProductTag(CoreModel):
-    product = models.ForeignKey("Product", on_delete=models.CASCADE)
-    # tag_id = models.ForeignKey("Tag")
