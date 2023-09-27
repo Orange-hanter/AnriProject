ddaccd1bee494df59c795545641017c19f60177b
Иван Бойко <63643593+ivanboika@users.noreply.github.com>
2023-09-27 18:59:00 +0300

    added other models in admin

--- api/anri/apps/orders/admin.py
+++ api/anri/apps/orders/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from anri.apps.orders.models import Order, OrderItem
 
-# Register your models here.
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("user", "order_date", "total_amount", "status")
+    list_filter = (
+        "order_date",
+        "user",
+    )
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ("order", "product", "quantity", "price_at_order_time", "subtotal")
+    list_filter = (
+        "order",
+        "product",
+    )
