b8d70ea1175fe7140f366c876b08919d91aff7a7
132dannys <Danlucky2003@gmail.com>
2023-10-02 10:45:04 +0300

    Add common pagination

--- api/anri/apps/products/urls.py
+++ api/anri/apps/products/urls.py
@@ -1,6 +1,4 @@
 from rest_framework import routers
-from django.conf import settings
-from django.conf.urls.static import static
 
 from .views import ProductViewSet, TagViewSet
 
@@ -10,6 +8,3 @@ router.register(r"products", ProductViewSet)
 router.register(r"tags", TagViewSet)
 
 urlpatterns = router.urls
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- api/anri/urls.py
+++ api/anri/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include, re_path
 
 
@@ -78,3 +79,6 @@ if "SWAGGER" in settings.ANRI_FEATURES:
     ]
 
 urlpatterns += swagger_urlpatterns
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
