7a3453bb708da671b7b0c9457fdaa743bbcee8d5
132dannys <Danlucky2003@gmail.com>
2023-10-03 17:51:32 +0300

    Refactor CartViewSet

--- api/anri/apps/carts/views.py
+++ api/anri/apps/carts/views.py
@@ -10,18 +10,20 @@ class CartViewSet(viewsets.ModelViewSet):
     queryset = CartItem.objects.all()
     serializer_class = CartItemSerializer
 
-    def list(self, request, *args, **kwargs):
-        queryset = CartItem.objects.filter(user=request.user).annotate(
+    def get_queryset(self):
+        queryset = CartItem.objects.filter(user=self.request.user).annotate(
             products_cost=F("product__price") * F("quantity")
         )
-        serializer = CartSerializer(queryset, many=True)
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "list" or self.action == "retrieve":
+            return CartSerializer
+        return CartItemSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
         response_data = {"result": serializer.data}
         response_data["total_cost"] = queryset.aggregate(total_cost=Sum("products_cost")).get("total_cost")
         return Response(response_data)
-
-    def retrieve(self, request, *args, **kwargs):
-        queryset = CartItem.objects.filter(user=request.user).annotate(
-            products_cost=F("product__price") * F("quantity")
-        )
-        serializer = CartSerializer(queryset, many=True)
-        return Response(serializer.data)
