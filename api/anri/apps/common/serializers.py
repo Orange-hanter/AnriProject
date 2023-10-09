c93dda88cbac5b66b2135cacc4f82e2960a886e3
132dannys <Danlucky2003@gmail.com>
2023-10-09 23:26:25 +0300

    Update djoser custom serializer

--- api/anri/apps/common/serializers.py
+++ api/anri/apps/common/serializers.py
@@ -1,5 +1,4 @@
 from djoser.serializers import UserCreateSerializer
-from django.contrib.auth.models import User
 
 from anri.enviroment import env
 
@@ -7,8 +6,8 @@ from anri.enviroment import env
 class CustomUserCreateSerializer(UserCreateSerializer):
     def save(self, **kwargs):
         if self.context["request"].user.email == env.str("ANRI_GUEST_USER_EMAIL"):
-            user = User.objects.filter(pk=self.context["request"].user.id)
+            user = self.context["request"].user
             user.is_active = False
-            user.save()
+            user.save(update_fields=("is_active",))
             self.instance = user
         return super().save(**kwargs)
