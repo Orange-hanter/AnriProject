edb0bce92cec85a67ac0151cc58b42208c33a32a
dannys <Danlucky2003@gmail.com>
2024-01-08 12:18:55 +0300

    Update cors settings

--- api/anri/settings/rest_framework.py
+++ api/anri/settings/rest_framework.py
@@ -1,3 +1,5 @@
+from anri.enviroment import env
+
 REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
     "DEFAULT_RENDER_CLASSES": [
@@ -12,5 +14,5 @@ REST_FRAMEWORK = {
 }
 
 
-# CORS_ALLOWED_ORIGINS = env.list("ANRI_CORS_ALLOWED_ORIGINS", default=[])
-# CORS_ALLOW_ALL_ORIGINS = env.bool("ANRI_CORS_ALLOW_ALL_ORIGINS", default=False)
+CORS_ALLOWED_ORIGINS = env.list("ANRI_CORS_ALLOWED_ORIGINS", default=[])
+CORS_ALLOW_ALL_ORIGINS = env.bool("ANRI_CORS_ALLOW_ALL_ORIGINS", default=False)
