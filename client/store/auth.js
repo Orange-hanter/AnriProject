ba15e4472603a2294a60176bb55522e47f5390bd
Novik-Artem <avacod222@gmail.com>
2024-02-25 10:29:23 +0300

    bugfix/jwt test commit

--- client/store/auth.js
+++ client/store/auth.js
@@ -19,36 +19,34 @@ export const actions = {
 
   async auth({ commit, dispatch }, { username, password }) {
     const value = await Auth.auth({ username, password })
-    commit('setToken', value.access)
     localStorage.setItem('accessToken', value.access)
     localStorage.setItem('refreshToken', value.refresh)
+    commit('setToken', value.access)
     dispatch('refreshTokenTimeout')
   },
 
   async refresh({ commit, dispatch }) {
-    const access = localStorage.getItem('accessToken')
-    const data = await Auth.verify(access)
-    commit('setToken', data)
-
     const refresh = localStorage.getItem('refreshToken')
     const value = await Auth.refresh(refresh)
-    commit('setToken', value.access)
+    commit('setToken', value)
     localStorage.setItem('accessToken', value.access)
-    localStorage.setItem('refreshToken', value.refresh)
     dispatch('refreshTokenTimeout')
   },
 
-  async verify({ commit }) {
-    const access = localStorage.getItem('accessToken')
-    const value = await Auth.verify(access)
-    commit('setToken', value)
-    // localStorage.setItem('refreshToken', value.refresh)
-    //dispatch('refreshTokenTimeout')
+  async verify() {
+    const refresh = localStorage.getItem('refreshToken')
+    const value = await Auth.verify(refresh)
+    console.log(value)
   },
 
   async refreshTokenTimeout({ dispatch }) {
     setTimeout(() => dispatch('refresh'), 0.25 * 60 * 1000)
+    setTimeout(() => dispatch('verify'), 0.25 * 60 * 1000)
   },
+
+  // async verifyTokenTimeout({ dispatch }) {
+  //   setTimeout(() => dispatch('verify'), 0.25 * 60 * 1000)
+  // },
 
   async activation(context, { uid, token }) {
     const value = await Auth.activation(uid, token)
