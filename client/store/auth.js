0ccfbe4135a08faccc54a50e2a1a92986f29f90a
Novik-Artem <avacod222@gmail.com>
2024-02-28 16:27:50 +0300

    fix bugs with jwt

--- client/store/auth.js
+++ client/store/auth.js
@@ -23,30 +23,46 @@ export const actions = {
     localStorage.setItem('refreshToken', value.refresh)
     commit('setToken', value.access)
     dispatch('refreshTokenTimeout')
+    dispatch('verifyTokenTimeout')
   },
 
   async refresh({ commit, dispatch }) {
     const refresh = localStorage.getItem('refreshToken')
     const value = await Auth.refresh(refresh)
+    if (!value.access) {
+      localStorage.removeItem('accessToken')
+      localStorage.removeItem('refreshToken')
+      commit('deleteToken')
+      location.reload()
+      return
+    }
     commit('setToken', value)
     localStorage.setItem('accessToken', value.access)
     dispatch('refreshTokenTimeout')
   },
 
-  async verify() {
+  async verify({ dispatch }) {
     const refresh = localStorage.getItem('refreshToken')
     const value = await Auth.verify(refresh)
-    console.log(value)
+    if (value.status !== 200) {
+      dispatch('refresh')
+    }
+    dispatch('verifyTokenTimeout')
   },
 
   async refreshTokenTimeout({ dispatch }) {
-    setTimeout(() => dispatch('refresh'), 0.25 * 60 * 1000)
-    setTimeout(() => dispatch('verify'), 0.25 * 60 * 1000)
+    setTimeout(() => dispatch('refresh'), 0.3 * 60 * 1000)
   },
 
-  // async verifyTokenTimeout({ dispatch }) {
-  //   setTimeout(() => dispatch('verify'), 0.25 * 60 * 1000)
-  // },
+  async verifyTokenTimeout({ dispatch }) {
+    setTimeout(() => dispatch('verify'), 0.1 * 60 * 1000)
+  },
+
+  async deleteToken({ commit }) {
+    localStorage.removeItem('accessToken')
+    localStorage.removeItem('refreshToken')
+    commit('deleteToken')
+  },
 
   async activation(context, { uid, token }) {
     const value = await Auth.activation(uid, token)
@@ -63,8 +79,10 @@ export const mutations = {
   setToken(state, value) {
     state.token = value
   },
-
   initToken(state) {
     state.token = localStorage.getItem('refreshToken') || null
+  },
+  deleteToken(state) {
+    state.token = null
   },
 }
